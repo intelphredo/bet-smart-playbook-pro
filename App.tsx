b8b5b727480bee0e5228b2062cf076116f20affe
intelphredo <freddiblanco@gmail.com>
2026-01-11 11:56:51 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AppRouter from "@/router/AppRouter";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import { QueryPersistence } from "@/providers/QueryPersistence";
 
 const App = () => {
   return (
     <ReactQueryProvider>
-      <AppRouter />
+      <QueryPersistence>
+        <AppRouter />
+      </QueryPersistence>
     </ReactQueryProvider>
   );
 };
