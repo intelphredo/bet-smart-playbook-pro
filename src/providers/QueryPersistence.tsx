7d78c5346e6b778805bec72a63ad831096cce3fc
intelphredo <freddiblanco@gmail.com>
2026-01-11 11:50:42 -0700

    Create QueryPersistence.tsx

---
+++ src/providers/QueryPersistence.tsx
@@ -0,0 +1,18 @@
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { queryClient } from "./ReactQueryProvider";
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+export const QueryPersistence = ({ children }) => {
+  return (
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      {children}
+    </PersistQueryClientProvider>
+  );
+};
