d28692fdf1651e4a6dfb2c0169bb28efb6920484
intelphredo <freddiblanco@gmail.com>
2026-01-11 11:26:49 -0700

    Update GamesPage.tsx

--- src/pages/GamesPage.tsx
+++ src/pages/GamesPage.tsx
@@ -1,24 +1,3 @@
-// src/pages/GamesPage.tsx
+import VirtualizedMatchList from "@/components/match/VirtualizedMatchList";
 
-import React from "react";
-import MatchList from "@/components/match/MatchList";
-import AutoRefreshIndicator from "@/components/match/AutoRefreshIndicator";
-import { useGames } from "@/hooks/useGames";
-
-const GamesPage: React.FC = () => {
-  const { games } = useGames();
-
-  return (
-    <div className="max-w-3xl mx-auto py-6 px-4">
-      <h1 className="text-2xl font-bold mb-4">All Games</h1>
-
-      {games.length > 0 && (
-        <AutoRefreshIndicator lastUpdated={games[0].lastUpdated} />
-      )}
-
-      <MatchList />
-    </div>
-  );
-};
-
-export default GamesPage;
+<VirtualizedMatchList games={games} />
